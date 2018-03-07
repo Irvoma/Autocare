c472057473de833cf3896c3245729c7466e68a05
Irvoma <37051301+Irvoma@users.noreply.github.com>
2018-03-06 23:12:13 -0600

    commit

--- app/src/main/java/com/irving/autocare/ActivityFormularioMedicamento.java
+++ app/src/main/java/com/irving/autocare/ActivityFormularioMedicamento.java
@@ -2,6 +2,10 @@ package com.irving.autocare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class ActivityFormularioMedicamento extends Activity {
 
@@ -9,5 +13,12 @@ public class ActivityFormularioMedicamento extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_medicamento);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tipoDosis,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
