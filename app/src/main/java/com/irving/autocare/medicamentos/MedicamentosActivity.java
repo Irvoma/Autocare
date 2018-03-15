fa29de6dfd89b62e342108df6e322fa0e95a9806
Irvoma <37051301+Irvoma@users.noreply.github.com>
2018-03-15 16:20:45 -0600

    commit

--- app/src/main/java/com/irving/autocare/medicamentos/MedicamentosActivity.java
+++ app/src/main/java/com/irving/autocare/medicamentos/MedicamentosActivity.java
@@ -8,54 +8,25 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.irving.autocare.R;
 
+import org.w3c.dom.Text;
+
 public class MedicamentosActivity extends AppCompatActivity {
     public static final String EXTRA_MEDICAMENTO_ID = "extra_medicamento_id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicamentos);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MedicamentosActivity.this);
-                View myView = getLayoutInflater().inflate(R.layout.formulario_medicamento, null);
-
-                Spinner spinner = (Spinner) myView.findViewById(R.id.spinner);
-
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.tipoDosis,
-                        android.R.layout.simple_spinner_item);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
-
-                mBuilder.setView(myView).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Aceptar", Toast.LENGTH_SHORT).show();
-
-                    }
-                }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Cancelar", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
-                AlertDialog dialog = mBuilder.create();
-                dialog.show();
-            }
-        });
 
         MedicamentosFragment fragment = (MedicamentosFragment)
                 getSupportFragmentManager().findFragmentById(R.id.medicamentos_container);
--- app/src/main/java/com/irving/autocare/medicamentos/MedicamentosFragment.java
+++ app/src/main/java/com/irving/autocare/medicamentos/MedicamentosFragment.java
@@ -1,16 +1,22 @@
 package com.irving.autocare.medicamentos;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.irving.autocare.R;
 import com.irving.autocare.data.MedicamentosDbHelper;
@@ -44,6 +50,50 @@ public class MedicamentosFragment extends Fragment {
 
         loadMedicamentos();
 
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getContext(), "Aceptar", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
+                View myView = getLayoutInflater().inflate(R.layout.formulario_medicamento, null);
+
+                Spinner spinner = (Spinner) myView.findViewById(R.id.spinner);
+
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.tipoDosis,
+                        android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                        String medida = (String) adapterView.getItemAtPosition(pos);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+                mBuilder.setView(myView).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //agregar medicamento
+                        Toast.makeText(getContext(), "Aceptar", Toast.LENGTH_SHORT).show();
+                    }
+                }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getContext(), "Cancelar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
+
         return root;
     }
 
@@ -69,4 +119,6 @@ public class MedicamentosFragment extends Fragment {
             }
         }
     }
+
+    private class
 }
